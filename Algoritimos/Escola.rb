8a0df9dc493260982affdf1387652d15bf2e4364
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2024-02-16 21:02:35 -0300

    update.02

--- Algoritimos/Escola.rb
+++ Algoritimos/Escola.rb
@@ -1,24 +1,25 @@
-    puts "-------------------------"
-    puts "  Escola do Madruguinha  "
-    puts "-------------------------"
-    
-    print   "Digite a primeira nota: "
-    n1 = Float(gets)
-    
-    print "Digite a segunda nota:"
-    n2 = Float(gets)
-    puts "-------------------------"
-    
-    if media = (n1 + n2) / 2
-    puts ("MEDIA: " + media.to_s)
-    
-    elsif media = 7 
-    puts "O ALUNO ESTA APROVADO"
-    puts "-------------------------"
-    
-    else  media = 7  
-    puts "O ALUNO ESTA REPROVADO"
+puts "-------------------------"
+puts "  Escola do Madruguinha  "
+puts "-------------------------"
 
-    end
+print   "Digite a primeira nota: "
+n1 = Float(gets)
 
-    puts "-------------------------"
+print "Digite a segunda nota:"
+n2 = Float(gets)
+puts "-------------------------"
+
+if media = (n1 + n2) / 2
+  puts ("MEDIA: " + media.to_s)
+
+  MEDIA = gets.chomp.to_i
+  case MEDIA
+  when 1..6
+  puts "O ALUNO ESTA REPROVADO"
+  puts "-------------------------"
+  when 7..9
+  puts "O ALUNO ESTA APROVADO"
+  puts "-------------------------"
+  else
+  puts 'Não foi possível identificar'
+  end
