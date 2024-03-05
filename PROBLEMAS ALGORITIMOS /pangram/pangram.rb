b0d16ec7aa80015884cbf61ee447b9e1a87ce096
Daniel Siqueira <danielbzdigital@outlook.com>
2024-03-05 17:52:15 -0300

    tentativa 01

--- PROBLEMAS ALGORITIMOS /pangram/pangram.rb
+++ PROBLEMAS ALGORITIMOS /pangram/pangram.rb
@@ -1,10 +1,16 @@
-module BookKeeping
-  VERSION = 3
+def missing_characters(sentence)
+  alphabet = ('a'..'z').to_a
+  missing = alphabet.reject { |char| sentence.downcase.include?(char) }
+  missing.join('')
 end
 
-class Pangram
-  def self.pangram?(sentence)
-    downcased_sentence = sentence.downcase
-    ('a'..'z').all? { |char| downcased_sentence.include?(char) }
-  end
+print "Digite uma frase:"
+input = gets.chomp
+
+missing = missing_characters(input)
+
+if missing.empty?
+  puts "A frase já é um Pangrama."
+else
+  puts "Caracteres faltando para tornar a frase um Pangrama: #{missing}"
 end
